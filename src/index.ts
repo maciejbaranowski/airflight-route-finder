216f6a50d908cd3457088ddb276980d3d039a32f
Maciej Baranowski <maciekbaranowski@onet.pl>
2019-01-21 19:31:31 +0100

    Port set for heroku deployment

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import travelAlgorithm from "./travelAlgorithm"
 import {filterNoDomestic, filterTooMuch, filterAirline} from "./filters";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const dataProvider = new DataProvider();
 
